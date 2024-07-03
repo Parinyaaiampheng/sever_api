1c3bfba46571737228b2a434c2b033b26d4f9fc7
Parinyaaiampheng <Parinya9037@gmail.com>
2024-07-03 09:43:14 +0700

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/api/users', (req, res) => {
     res.json(users);
 });
@@ -12,6 +15,31 @@ app.get('/api/users', (req, res) => {
 
 app.get("/api/users", (req, res) => {
     res.send("Hello! My name is Parinya.");
+});
+
+app.post('/api/users', (req, res) => {
+    users.push(req.body);
+    let json = req.body;
+    console.log(json);
+    res.send("Username: " + json.username + " inserted.");
+});
+
+app.put('/api/users/:id', (req, res) => {
+
+    const updateIndex = users.findIndex(user => user.id ===
+        Number(req.params.id))
+
+    res.send(`Update user id: '${users[updateIndex].id}' completed.`);
+
+});
+
+app.delete('/api/users/:id', (req, res) => {
+
+    const deletedIndex = users.findIndex(user => user.id ===
+        Number(req.params.id))
+
+    res.send(`Delete user '${users[deletedIndex].username}' completed.`);
+
 });
 
 
